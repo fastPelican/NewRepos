d3093706e5a893336f8a794beadd4bffc70df15d
Jonathan Wyckoff <jonathan.wyckoff@okstate.edu>
2021-02-09 12:18:36 -0600

    Add project files.

---
+++ JW Basic App GitHub/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JW_Basic_App_GitHub
+{
+    public class Class1
+    {
+        public string Name;
+        public Class1() 
+        {
+            Name = "[Jonathan Wyckoff] - The Code Master"; //Insert your name and moniker
+        }
+        
+    }
+}
